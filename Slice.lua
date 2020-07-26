e9cb2b66232ad13fb66dda6e87fa2f3eca5a4ff6
Jawn <65871382+hostacc@users.noreply.github.com>
2020-07-26 18:53:31 -0400

    Update Slice.lua

--- Slice.lua
+++ Slice.lua
@@ -1,2 +1,17 @@
+newcclosure = newcclosure or protect_function or function(f) return f end;
 game:GetService("Players").LocalPlayer:Kick("Autofarm discontinued for now")
-while true do end -- Just in case kick gets bypassed by a hookfunction/newcclosure method
+
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    if getnamecallmethod() == "Kick" then
+        while true do end
+    end
+    return old(self, ...)
+end)
+
+hookfunction(game:GetService("Players").LocalPlayer.Kick, newcclosure(function() 
+    while true do end
+end))
